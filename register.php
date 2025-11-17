3332ba1d7fafe9643be90a721af45ffe7765cffc
Pandouzz <adriancretzer@gmail.com>
2025-11-17 13:09:31 +0100

    Registrierung im Backend implementiert

---
+++ register.php
@@ -0,0 +1,49 @@
+<?php
+header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Methods: POST");
+
+require_once __DIR__ . '/database.php';
+
+$input = json_decode(file_get_contents("php://input"), true);
+
+if (!$input) {
+    echo json_encode(["success" => false, "message" => "Invalid JSON"]);
+    exit;
+}
+
+$db = new Database();
+$conn = $db->getConnection();
+
+try {
+    $stmt = $conn->prepare("
+        INSERT INTO users
+        (first_name, last_name, email, phone_number, street_name, street_number, zip_code, city, password_hash, salt, is_provider)
+        VALUES
+        (:first_name, :last_name, :email, :phone_number, :street_name, :street_number, :zip_code, :city, :password_hash, :salt, 0)
+    ");
+
+    $salt = bin2hex(random_bytes(16));
+    $password_hash = hash("sha256", $input["password"] . $salt);
+
+    $stmt->execute([
+        ":first_name" => $input["firstName"],
+        ":last_name" => $input["lastName"],
+        ":email" => $input["email"],
+        ":phone_number" => $input["phoneNumber"],
+        ":street_name" => $input["streetName"],
+        ":street_number" => $input["streetNumber"],
+        ":zip_code" => $input["zipCode"],
+        ":city" => $input["city"],
+        ":password_hash" => $password_hash,
+        ":salt" => $salt
+    ]);
+    echo json_encode(["success" => true]);
+} catch (PDOException $e) {
+    echo json_encode([
+        "success" => false,
+        "message" => $e->getMessage()
+    ]);
+}
+?>
