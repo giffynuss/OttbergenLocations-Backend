2397c68c36e130570dd60529c725b9485d68d9ee
Pandouzz <adriancretzer@gmail.com>
2025-11-17 12:36:03 +0100

    Datenbankanbindung

---
+++ test_db.php
@@ -0,0 +1,12 @@
+<?php
+require_once __DIR__ . '/config/database.php';
+
+$db = new Database();
+$conn = $db->getConnection();
+
+if ($conn) {
+    echo "<h2 style='color: green;'>✔ Database connection successful!</h2>";
+} else {
+    echo "<h2 style='color: red;'>✘ Database connection failed!</h2>";
+}
+?>
