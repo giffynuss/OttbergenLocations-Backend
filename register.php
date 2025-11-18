212a6968511ef857730361c9af36bf63fbb22867
Pandouzz <adriancretzer@gmail.com>
2025-11-18 10:21:41 +0100

    Die dumme Registrierung funktioniert endlich

--- register.php
+++ register.php
@@ -4,7 +4,7 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Access-Control-Allow-Methods: POST");
 
-require_once __DIR__ . '/../../config/database.php';
+require_once __DIR__ . '/database.php';
 
 $input = json_decode(file_get_contents("php://input"), true);
 
