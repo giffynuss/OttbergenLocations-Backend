1b529ae8d61203541e7c82a82f990c9e16b74372
Pandouzz <adriancretzer@gmail.com>
2025-11-19 12:45:08 +0100

    Dumme Ordnerstruktur ist unvollständig

---
+++ me.php
@@ -0,0 +1,9 @@
+<?php
+header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Authorization, Content-Type");
+header("Access-Control-Allow-Methods: GET");
+
+require_once __DIR__ . "/config/database.php";
+
+?>
